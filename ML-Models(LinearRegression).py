8b2279ac4cddb06a41c435fa15d4e2e0b21f27fc
Gaurav Gurjar <gujjargaurav712@gmail.com>
2026-06-04 14:47:50 +0530

    Add files via upload
    Linear Regression is a supervised machine learning algorithm used to predict a continuous numerical value by finding the best-fit straight line between input variables and the target variable.
    
    Training Data
          ↓
    Fit Best-Fit Line
          ↓
    Learn Relationship
          ↓
    Predict New Values

---
+++ ML-Models(LinearRegression).py
@@ -0,0 +1,44 @@
+import pandas as pd 
+
+df = pd.read_csv(r"insurance - insurance.csv")
+# print(df.head(3))
+
+x = df.drop(columns=['charges'])
+y = df['charges']
+
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+
+x['sex']=le.fit_transform(x['sex'])
+x['smoker']=le.fit_transform(x['smoker'])
+x['region']=le.fit_transform(x['region'])
+
+# print(x.head(3))
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+
+from sklearn.linear_model import LinearRegression
+
+lr = LinearRegression()
+
+lr.fit(x_train, y_train)
+
+y_pred = lr.predict(x_test)
+
+
+from sklearn.metrics import r2_score
+
+print(r2_score(y_test, y_pred) )
+
+
+
+
+
+
+
+
+
